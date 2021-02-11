cf8820171b743f921064ed65245ff0f4eccb17ec
Isma Yurika <60314411+ismayurika@users.noreply.github.com>
2021-02-11 10:30:47 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Tindak Pidana Kantor Kejaksaan Negeri Kisaran</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/kejarikisaran.jpeg"></img></p>
+                          <p class="label-title" align="center"><strong>KEJAKSAAN NEGERI KISARAN</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Arsip Kejaksaan Negeri Kisaran.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data pidana umum, pengelolaan data pidana khusus, <br>
+                            dan juga pengelolaan laporan pidana umum dan pengolaan laporan pidana khusus.<br><br>
+                            
+                            Nama Kelompok<br>
+                            1. ISMA YURIKA TANJUNG <br>
+                            2. RIZA UMAMI RITONGA <br>
+                            3. RIZKA AMIRAH FILZA TANJUNG <br>
+                            <br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
